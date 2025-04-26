6f503879ff9e36a4a9d16d3fc605cb15cde024f1
allenlin <ht016746@gmail.com>
2025-04-26 08:41:02 +0700

    chore: login form

--- apps/erify_production/src/pages/login.tsx
+++ apps/erify_production/src/pages/login.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@eridu/ui/components/button";
+import { LoginForm } from "@eridu/ui/components/login-form";
+import { useCallback } from "react";
+
 export const LoginPage: React.FC = () => {
-  return <>Login</>;
+  const onSubmit = useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }, []);
+
+  return <div className="h-screen w-full flex justify-center items-center"><LoginForm className="min-w-80" onSubmit={onSubmit} forgetPassword={<Button variant="link" type="button" className="ml-auto pr-0">Forget your password?</Button>} /></div>;
 };
 
 export default LoginPage;
