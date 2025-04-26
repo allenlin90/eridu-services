0c6cf1e3a33bed4c17a14b71a9d2c18b532f7107
allenlin <ht016746@gmail.com>
2025-04-26 08:41:02 +0700

    chore: login form

--- apps/erify_production/src/app.tsx
+++ apps/erify_production/src/app.tsx
@@ -1,6 +1,7 @@
 import { PrivateRouteGuard } from "@/auth/components/private-route-guard";
 import { PublicRouteGuard } from "@/auth/components/public-route-guard";
 import { Layout } from "@/components/layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 import { Navigate, Route, Routes } from "react-router";
 
@@ -8,20 +9,24 @@ import ErrorFallback from "./components/error-fallback";
 import Dashboard from "./pages/dashboard";
 import LoginPage from "./pages/login";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="login" element={<PublicRouteGuard />}>
-            <Route index element={<LoginPage />} />
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="login" element={<PublicRouteGuard />}>
+              <Route index element={<LoginPage />} />
+            </Route>
+            <Route path="/" element={<PrivateRouteGuard />}>
+              <Route index element={<Dashboard />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
-          <Route path="/" element={<PrivateRouteGuard />}>
-            <Route index element={<Dashboard />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
--- apps/erify_production/src/main.tsx
+++ apps/erify_production/src/main.tsx
@@ -1,16 +1,11 @@
-import { AuthProvider } from "@eridu/auth-service/providers/auth-provider";
+import { baseURL } from "@/auth/lib";
 import "@eridu/ui/globals.css";
+import { AuthProvider } from "@eridu/auth-service/providers/auth-provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import App from "./app";
-
-const baseURL = import.meta.env.VITE_AUTH_BASE_URL;
-
-if (!baseURL) {
-  throw new Error("AUTH_BASE_URL is not defined");
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
