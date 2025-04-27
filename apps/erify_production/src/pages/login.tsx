96db325a58469da121db9f4f97762fb65bef6256
allenlin <ht016746@gmail.com>
2025-04-27 08:29:37 +0700

    chore: todo for handle login error

--- apps/erify_production/src/pages/login.tsx
+++ apps/erify_production/src/pages/login.tsx
@@ -1,35 +1,17 @@
-import { useLogin } from "@eridu/auth-service/hooks/use-login";
+import { useLogin } from "@/auth/hooks/use-login";
 import { Button } from "@eridu/ui/components/button";
 import { LoginForm } from "@eridu/ui/components/login-form";
-import { useCallback } from "react";
-import { useNavigate } from "react-router";
 
 export const LoginPage: React.FC = () => {
-  const navigate = useNavigate();
-  const { login, loading } = useLogin();
-
-  const onSubmit = useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    const credentials = Object.fromEntries(formData.entries()) as {
-      email: string;
-      password: string;
-    };
-
-    const res = await login(credentials);
-
-    if (res) {
-      navigate("/");
-    }
-  }, [login, navigate]);
+  const { error, loading, login } = useLogin();
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <LoginForm
         className="min-w-80"
         disabled={loading}
-        onSubmit={onSubmit}
+        error={error}
+        onSubmit={login}
         forgetPassword={(
           <Button variant="link" type="button" className="ml-auto pr-0">
             Forget your password?
