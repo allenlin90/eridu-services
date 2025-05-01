c9cdb83041769ee17549d97fa6d97fea2645f3c5
allenlin <ht016746@gmail.com>
2025-05-01 23:10:03 +0700

    chore: use shows details hook

--- apps/erify_production/src/pages/show.tsx
+++ apps/erify_production/src/pages/show.tsx
@@ -1,7 +1,20 @@
-// import { useParams } from "react-router";
+import { useShowDetails } from "@/shows/hooks/use-show-details";
+import { LoaderCircle } from "lucide-react";
+import { useParams } from "react-router";
 
 export const Show: React.FC = () => {
-  // const { show_uid } = useParams();
+  const { show_uid } = useParams();
+  const { isPending } = useShowDetails(show_uid);
+
+  if (isPending) {
+    return (
+      <div className="flex-1 flex justify-center items-center">
+        <div>
+          <LoaderCircle className="animate-spin" />
+        </div>
+      </div>
+    );
+  }
 
   return null;
 };
