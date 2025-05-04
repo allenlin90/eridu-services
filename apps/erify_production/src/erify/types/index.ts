5fe0a8716c051d61c5eeeb1523c2dc4800c741e1
allenlin <ht016746@gmail.com>
2025-05-04 23:55:51 +0700

    chore: use query hook

---
+++ apps/erify_production/src/erify/types/index.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const BrandSchema = z.object({
+  uid: z.string(),
+  name: z.string(),
+});
+
+export const PlatformSchema = z.object({
+  uid: z.string(),
+  name: z.string(),
+});
+
+export const ShowPlatformSchema = z.object({
+  uid: z.string(), // show_platform_mc
+  is_active: z.boolean(),
+  alias_id: z.string().nullable(),
+});
+
+export const ShowSchema = z.object({
+  uid: z.string(),
+  name: z.string(),
+  start_time: z.string(),
+  end_time: z.string(),
+});
+
+export const StudioRoomSchema = z.object({
+  uid: z.string(),
+  name: z.string(),
+  type: z.enum(["s", "m", "l"]).nullable(),
+});
+
+export type Brand = z.infer<typeof BrandSchema>;
+export type Platform = z.infer<typeof PlatformSchema>;
+export type ShowPlatform = z.infer<typeof ShowPlatformSchema>;
+export type Show = z.infer<typeof ShowSchema>;
+export type StudioRoom = z.infer<typeof StudioRoomSchema>;
