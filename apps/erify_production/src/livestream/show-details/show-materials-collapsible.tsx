fa4ba0a80fbe8dccf46ca1431fe5d311a78002bb
allenlin <ht016746@gmail.com>
2025-05-05 14:39:13 +0700

    feat: show details

---
+++ apps/erify_production/src/livestream/show-details/show-materials-collapsible.tsx
@@ -0,0 +1,79 @@
+import type { LucideProps } from "lucide-react";
+
+import { Button } from "@eridu/ui/components/button";
+import { Card, CardContent, CardHeader } from "@eridu/ui/components/card";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@eridu/ui/components/collapsible";
+import { cn } from "@eridu/ui/lib/utils";
+import { ChevronRight, CircleDollarSign, FileText, ImageIcon, Layers } from "lucide-react";
+
+import type { ShowMaterial } from "../shows/types/show-materials";
+
+const getIcon = (type: string, { className, ...props }: LucideProps = {}) => {
+  const size = "h-4 w-4";
+
+  switch (type) {
+    case "script":
+      return <FileText className={cn(size, className)} {...props} />;
+    case "scene":
+      return <ImageIcon className={cn(size, className)} {...props} />;
+    case "mechanic":
+      return <CircleDollarSign className={cn(size, className)} {...props} />;
+    case "obs_layer":
+      return <Layers className={cn(size, className)} {...props} />;
+    default:
+      return <FileText className={cn(size, className)} {...props} />;
+  }
+};
+
+type ShowMaterialsCollapsibleProps = {
+  showMaterials?: ShowMaterial[];
+};
+
+export const ShowMaterialsCollapsible: React.FC<ShowMaterialsCollapsibleProps> = ({
+  showMaterials,
+}) => {
+  if (!showMaterials)
+    return null;
+
+  return (
+    <Collapsible className="group/collapsible flex flex-col gap-2">
+      <CollapsibleTrigger asChild>
+        <Button variant="ghost" className="w-full px-0">
+          <span>Materials</span>
+          <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+        </Button>
+      </CollapsibleTrigger>
+      <CollapsibleContent className="flex flex-col gap-4">
+        {showMaterials.map((material) => {
+          return (
+            <Card key={material.uid}>
+              <CardHeader>
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center gap-2">
+                    {getIcon(material.type)}
+                    <span className="capitalize font-medium">{material.type}</span>
+                  </div>
+                  {material.resource_url && (
+                    <Button variant="outline" asChild>
+                      <a
+                        href={material.resource_url}
+                        target="_blank"
+                        referrerPolicy="no-referrer"
+                      >
+                        Open
+                      </a>
+                    </Button>
+                  )}
+                </div>
+              </CardHeader>
+              <CardContent className="text-sm">
+                <p>{material.name}</p>
+                <p>{material.description}</p>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </CollapsibleContent>
+    </Collapsible>
+  );
+};
--- apps/erify_production/src/pages/livestream/show.tsx
+++ apps/erify_production/src/pages/livestream/show.tsx
@@ -1,12 +1,20 @@
+import { ShowDetailsCard } from "@/livestream/show-details/show-details-card";
+import { useMaterials } from "@/livestream/shows/hooks/use-materials";
 import { useShowDetails } from "@/livestream/shows/hooks/use-show-details";
 import { LoaderCircle } from "lucide-react";
 import { useParams } from "react-router";
 
 export const Show: React.FC = () => {
   const { show_uid } = useParams();
-  const { isPending } = useShowDetails(show_uid);
+  const { isPending: isLoadingShowDetails, data: showDetails } = useShowDetails(show_uid);
+  const { isPending: isLoadingMaterials, data: showMaterials } = useMaterials(show_uid);
 
-  if (isPending) {
+  if (!showDetails) {
+    // TODO: implement not found
+    return null;
+  }
+
+  if (isLoadingShowDetails || isLoadingMaterials) {
     return (
       <div className="flex-1 flex justify-center items-center">
         <div>
@@ -16,8 +24,11 @@ export const Show: React.FC = () => {
     );
   }
 
-  // TODO: useMaterials hook to query show-platform-materials and details
-  return null;
+  return (
+    <div className="p-4">
+      <ShowDetailsCard showDetails={showDetails} showMaterials={showMaterials} />
+    </div>
+  );
 };
 
 export default Show;
