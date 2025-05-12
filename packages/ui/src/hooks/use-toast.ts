a2ffc8282c9f2d0dfd274717c11e438da643fc13
allenlin <ht016746@gmail.com>
2025-05-12 23:24:34 +0700

    chore: toast timeout in 10 sec

--- packages/ui/src/hooks/use-toast.ts
+++ packages/ui/src/hooks/use-toast.ts
@@ -9,7 +9,7 @@ import type {
 } from "@eridu/ui/components/toast";
 
 const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_REMOVE_DELAY = 10000;
 
 type ToasterToast = ToastProps & {
   id: string;
