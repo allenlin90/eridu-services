8594f24795c8c0acdb9fddf688e49d1c0705aea4
allenlin <ht016746@gmail.com>
2025-05-12 23:58:00 +0700

    refactor: useMutation hook

--- apps/erify_production/src/pages/reset-password.tsx
+++ apps/erify_production/src/pages/reset-password.tsx
@@ -1,6 +1,6 @@
 import { ResetPasswordForm } from "@/auth/components/forms/reset-password-form";
+import { useResetPassword } from "@/auth/hooks/use-reset-password";
 import { ROUTES } from "@/constants/routes";
-import useSession from "@eridu/auth-service/hooks/use-session";
 import { Card, CardContent, CardHeader, CardTitle } from "@eridu/ui/components/card";
 import { useToast } from "@eridu/ui/hooks/use-toast";
 import { useCallback, useMemo } from "react";
@@ -9,7 +9,7 @@ import { Navigate, useNavigate, useSearchParams } from "react-router";
 type ResetPassword = React.ComponentProps<typeof ResetPasswordForm>["onSubmit"];
 
 export const ResetPasswordPage: React.FC = () => {
-  const { authClient } = useSession();
+  const { mutateAsync, isPending } = useResetPassword();
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const { toast } = useToast();
@@ -18,8 +18,12 @@ export const ResetPasswordPage: React.FC = () => {
 
   const onSubmit: ResetPassword
   = useCallback(async ({ password }) => {
-    const res = await authClient.resetPassword({ newPassword: password, token });
+    const res = await mutateAsync({ password, token });
     if (res.data?.status) {
+      toast({
+        variant: "success",
+        description: "Password is reset",
+      });
       return navigate(ROUTES.LOGIN);
     }
 
@@ -30,7 +34,7 @@ export const ResetPasswordPage: React.FC = () => {
       });
       return navigate(ROUTES.FORGET_PASSWORD);
     }
-  }, [authClient, navigate, toast, token]);
+  }, [mutateAsync, navigate, toast, token]);
 
   if (!token) {
     return <Navigate to={ROUTES.LOGIN} />;
@@ -43,7 +47,7 @@ export const ResetPasswordPage: React.FC = () => {
           <CardTitle className="text-2xl">Reset Password</CardTitle>
         </CardHeader>
         <CardContent>
-          <ResetPasswordForm className="min-w-80" onSubmit={onSubmit} />
+          <ResetPasswordForm className="min-w-80" disabled={isPending} onSubmit={onSubmit} />
         </CardContent>
       </Card>
     </div>
