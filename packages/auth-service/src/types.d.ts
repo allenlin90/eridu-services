a8c37855202350033e3da0c30447d2cb241ada67
allenlin <ht016746@gmail.com>
2025-05-13 23:31:28 +0700

    chore: fix User type issue

--- packages/auth-service/src/types.d.ts
+++ packages/auth-service/src/types.d.ts
@@ -1,14 +1,15 @@
+import type { userSchema } from "better-auth/db";
 import type { memberSchema, organizationSchema, teamSchema } from "better-auth/plugins";
 import type { JWTPayload } from "jose";
 import type { z } from "zod";
 
-export type { User } from "better-auth";
-
+export type User = z.infer<typeof userSchema>;
 export type Member = z.infer<typeof memberSchema>;
 export type Organization = z.infer<typeof organizationSchema>;
 export type Team = z.infer<typeof teamSchema>;
 export type Membership = (Member & { organization: Organization; team: Team });
 
 export type Session = JWTPayload & User & {
+  activeOrganizationId: string | null;
   memberships: Membership[];
 };
