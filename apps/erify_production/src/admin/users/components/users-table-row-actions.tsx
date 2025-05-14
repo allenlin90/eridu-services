3b71032ac75cba1f1f248cebac9d6d0477737a9d
allenlin <ht016746@gmail.com>
2025-05-14 10:23:16 +0700

    chore: show success toast when copy user id

--- apps/erify_production/src/admin/users/components/users-table-row-actions.tsx
+++ apps/erify_production/src/admin/users/components/users-table-row-actions.tsx
@@ -1,6 +1,7 @@
 import type { UserWithRole } from "@eridu/auth-service/types";
 
 import { RowActions } from "@eridu/ui/components/table/row-actions";
+import { useToast } from "@eridu/ui/hooks/use-toast";
 import { useCallback } from "react";
 
 type UsersTableRowActionsProps = {
@@ -8,12 +9,17 @@ type UsersTableRowActionsProps = {
 };
 
 export const UsersTableRowActions: React.FC<UsersTableRowActionsProps> = ({ user }) => {
+  const { toast } = useToast();
   const copyUserId = useCallback(
     (user_uid: string) =>
       (_e: React.MouseEvent<HTMLDivElement>) => {
         navigator.clipboard.writeText(user_uid);
+        toast({
+          variant: "success",
+          description: `User ID: ${user_uid} is copied`,
+        });
       },
-    [],
+    [toast],
   );
 
   const actions = [
