b747d8467de8d89e2cb6d1abfd7168f245e0fb7f
allenlin <ht016746@gmail.com>
2025-05-14 21:53:49 +0700

    refactor: module components

--- apps/erify_production/src/pages/admin/users.tsx
+++ apps/erify_production/src/pages/admin/users.tsx
@@ -1,54 +1,10 @@
 import { AddUserForm } from "@/admin/users/components/forms/add-user-form";
 import { UserSearchFilters } from "@/admin/users/components/user-search-filters";
-import { useAdminUserColumns } from "@/admin/users/hooks/use-admin-user-columns";
-import { useQueryUsers } from "@/admin/users/hooks/use-query-users";
+import { UsersTable } from "@/admin/users/components/users-table";
 import { FullPage } from "@/components/hoc/full-page";
 import { Modal } from "@/components/modal";
-import { Pagination } from "@/components/pagination";
 import { Button } from "@eridu/ui/components/button";
-import DataTable from "@eridu/ui/components/data-table";
-import { LoaderCircle, Plus } from "lucide-react";
-
-// TODO: allow users to change the limit
-const LIMIT = 10;
-
-const UsersPageContent: React.FC = () => {
-  const { data, isPending, isError, error } = useQueryUsers();
-  const columns = useAdminUserColumns();
-
-  if (isPending) {
-    return (
-      <div className="flex-1 flex justify-center items-center">
-        <div>
-          <LoaderCircle className="animate-spin" />
-        </div>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return <p className="text-center">{error?.message || "something went wrong"}</p>;
-  }
-
-  if (!data) {
-    return <p className="text-center">No data</p>;
-  }
-
-  const page = "offset" in data && data.offset !== undefined && data.limit !== undefined
-    ? Math.ceil(data.offset / data.limit) + 1
-    : 1;
-
-  return (
-    <>
-      <div className="max-w-full overflow-auto h-full max-h-sm-user-content-area sm:max-h-user-content-area">
-        <DataTable columns={columns} data={data.users} />
-      </div>
-      <div className="p-4">
-        <Pagination pageSize={LIMIT} page={page} total={data?.total ?? 1} />
-      </div>
-    </>
-  );
-};
+import { Plus } from "lucide-react";
 
 const UsersPage: React.FC = () => {
   return (
@@ -72,7 +28,7 @@ const UsersPage: React.FC = () => {
           <AddUserForm />
         </Modal>
       </div>
-      <UsersPageContent />
+      <UsersTable />
     </div>
   );
 };
