2743cfc80d40642fe75f74c6a0192048e2759d52
allenlin <ht016746@gmail.com>
2025-05-19 11:05:28 +0700

    feat: admin can add new brand

---
+++ apps/erify_production/src/erify/admin/brands/components/forms/add-new-brand.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@eridu/ui/components/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@eridu/ui/components/form";
+import { Input } from "@eridu/ui/components/input";
+import { useToast } from "@eridu/ui/hooks/use-toast";
+import { cn } from "@eridu/ui/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQueryClient } from "@tanstack/react-query";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { useAddBrand } from "../../hooks/use-add-brand";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+export type FormSchema = z.infer<typeof formSchema>;
+
+type AddNewBrandFormProps = {
+  cancel?: () => void | Promise<void>;
+} & React.ComponentProps<"form">;
+
+export const AddNewBrandForm: React.FC<AddNewBrandFormProps> = ({ className, cancel, ...props }) => {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const { isPending, mutateAsync } = useAddBrand({
+    onSuccess: ({ name }) => {
+      queryClient.invalidateQueries({ queryKey: ["brands"] });
+      toast({
+        variant: "success",
+        description: `Brand ${name} is created`,
+      });
+      cancel?.();
+    },
+  });
+
+  const submit = useCallback(async ({ name }: FormSchema) => {
+    await mutateAsync({ name });
+  }, [mutateAsync]);
+
+  return (
+    <Form {...form}>
+      <form
+        {...props}
+        className={cn("flex flex-col gap-2", className)}
+        onSubmit={form.handleSubmit(submit)}
+      >
+        <FormField
+          {...form.register("name")}
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <FormControl>
+                <Input type="text" disabled={isPending} {...field} />
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="w-full flex justify-end gap-4">
+          <Button
+            type="button"
+            variant="outline"
+            onClick={cancel}
+          >
+            Cancel
+          </Button>
+          <Button>Add</Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
--- apps/erify_production/src/pages/erify/admin/brands.tsx
+++ apps/erify_production/src/pages/erify/admin/brands.tsx
@@ -1,11 +1,9 @@
 import FullPage from "@/components/hoc/full-page";
-import Modal from "@/components/modal";
 import { PaginatedDataTable } from "@/components/paginated-data-table";
-import { BrandSearchFilters } from "@/erify/admin/users/components/brand-search-filters";
-import { useAdminBrandColumns } from "@/erify/admin/users/hooks/use-admin-brand-columns";
-import { useQueryBrands } from "@/erify/admin/users/hooks/use-query-brands";
-import { Button } from "@eridu/ui/components/button";
-import { Plus } from "lucide-react";
+import { AddBrandModal } from "@/erify/admin/brands/components/add-brand-modal";
+import { BrandSearchFilters } from "@/erify/admin/brands/components/brand-search-filters";
+import { useAdminBrandColumns } from "@/erify/admin/brands/hooks/use-admin-brand-columns";
+import { useQueryBrands } from "@/erify/admin/brands/hooks/use-query-brands";
 
 const Brands: React.FC = () => {
   const { data, error, isPending, isError } = useQueryBrands();
@@ -15,20 +13,7 @@ const Brands: React.FC = () => {
     <div className="h-full p-4 pb-0 flex flex-col">
       <div className="max-w-full flex flex-col sm:flex-row gap-4 mb-4 sm:mb-0">
         <BrandSearchFilters className="w-full order-2 sm:order-1" />
-        <Modal
-          title="Add new brand"
-          description="Fill in the details to create a brand"
-          trigger={(
-            <Button
-              type="button"
-              variant="default"
-              className="w-full sm:w-min order-1 sm:order-2"
-            >
-              <Plus />
-              <span>Add Brand</span>
-            </Button>
-          )}
-        />
+        <AddBrandModal />
       </div>
       <PaginatedDataTable
         columns={columns}
