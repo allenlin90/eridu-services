0ab914715db8fba160d1fd36ba0a959200438e44
allenlin <ht016746@gmail.com>
2025-05-19 10:19:25 +0700

    feat: admin can search brands by name

--- apps/erify_production/src/erify/admin/users/hooks/use-query-brands.ts
+++ apps/erify_production/src/erify/admin/users/hooks/use-query-brands.ts
@@ -3,18 +3,28 @@ import type { Brand } from "@/erify/types";
 import type { UseQueryOptions } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 
+import { usePaginationParams } from "@/hooks/use-pagination-params";
 import { usePrivateAxios } from "@/hooks/use-private-axios";
 import { useQuery } from "@tanstack/react-query";
+
+import useBrandSearchParams from "./use-brand-search-params";
 
 type PaginatedBrands = PaginatedData<Brand>;
 
 export const useQueryBrands = (options?: UseQueryOptions<PaginatedBrands, AxiosError<{ message?: string }>>) => {
   const axios = usePrivateAxios();
+  const { params: paginationParams } = usePaginationParams();
+  const { params: brandSearchParams } = useBrandSearchParams();
 
   return useQuery({
-    queryKey: ["brands"],
+    queryKey: ["brands", paginationParams, brandSearchParams],
     queryFn: async () => {
-      const res = await axios.get<PaginatedBrands>("/admin/brands");
+      const res = await axios.get<PaginatedBrands>("/admin/brands", {
+        params: {
+          ...paginationParams,
+          ...brandSearchParams,
+        },
+      });
 
       return res.data;
     },
