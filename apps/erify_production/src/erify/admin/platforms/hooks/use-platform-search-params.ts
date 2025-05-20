22057e36063dadeffa363cb8ed3bdf2fe00eef25
allenlin <ht016746@gmail.com>
2025-05-20 11:36:49 +0700

    feat: admin can query and add platforms

---
+++ apps/erify_production/src/erify/admin/platforms/hooks/use-platform-search-params.ts
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router";
+
+export const usePlatformSearchParams = () => {
+  const [searchParams] = useSearchParams();
+
+  return useMemo(() => {
+    return {
+      params: {
+        name: searchParams.get("name"),
+      },
+      error: null,
+    };
+  }, [searchParams]);
+};
+
+export default usePlatformSearchParams;
