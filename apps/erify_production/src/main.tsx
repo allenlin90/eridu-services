2727f3a2122021825e2bc4929ba5f1d86afaa513
allenlin <ht016746@gmail.com>
2025-05-20 10:50:28 +0700

    chore: keep previous data for query

--- apps/erify_production/src/main.tsx
+++ apps/erify_production/src/main.tsx
@@ -2,7 +2,7 @@ import { baseURL } from "@/auth/lib";
 import "@eridu/ui/globals.css";
 import { createIDBPersister } from "@/lib/create-idb-persister";
 import { AuthProvider } from "@eridu/auth-service/providers/auth-provider";
-import { QueryClient } from "@tanstack/react-query";
+import { keepPreviousData, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { StrictMode } from "react";
@@ -12,10 +12,12 @@ import { BrowserRouter } from "react-router";
 import App from "./app";
 import { MembershipProvider } from "./providers/membership-provider";
 
+// TODO: optimize when query with pagination search params
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      placeholderData: keepPreviousData,
     },
   },
 });
