575e585e33ae1a27c8ea84aa0c1bd76054da14e6
allenlin <ht016746@gmail.com>
2025-05-20 08:17:02 +0700

    feat: erify admin can delete user

---
+++ apps/erify_production/src/erify/admin/users/hooks/use-remove-user.ts
@@ -0,0 +1,18 @@
+import type { UseMutationOptions } from "@tanstack/react-query";
+import type { AxiosError } from "axios";
+
+import { API_ENDPOINTS } from "@/constants/api-endpoints";
+import usePrivateAxios from "@/hooks/use-private-axios";
+import { useMutation } from "@tanstack/react-query";
+
+export const useRemoveUser = (options?: UseMutationOptions<void, AxiosError<{ message?: string }>, string>) => {
+  const axios = usePrivateAxios();
+
+  return useMutation({
+    mutationKey: ["remove-user"],
+    mutationFn: async (uid: string) => {
+      await axios.delete(`${API_ENDPOINTS.ADMIN.USERS}/${uid}`);
+    },
+    ...options,
+  });
+};
