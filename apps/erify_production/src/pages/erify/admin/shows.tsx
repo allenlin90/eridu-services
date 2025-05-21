c10ad79bece8d36f48c703266e433b9dc25f77f1
allenlin <ht016746@gmail.com>
2025-05-21 07:25:11 +0700

    feat: erify admin can read shows

--- apps/erify_production/src/pages/erify/admin/shows.tsx
+++ apps/erify_production/src/pages/erify/admin/shows.tsx
@@ -1,5 +1,25 @@
-export const Shows: React.FC = () => {
-  return null;
+import { FullPage } from "@/components/hoc/full-page";
+import { PaginatedDataTable } from "@/components/paginated-data-table";
+import { useAdminShowColumns } from "@/erify/admin/shows/hooks/use-admin-show-columns";
+import { useQueryShows } from "@/erify/admin/shows/hooks/use-query-shows";
+
+const Shows: React.FC = () => {
+  const { data, error, isPending, isError } = useQueryShows();
+  const columns = useAdminShowColumns();
+
+  return (
+    <div className="h-full p-4 pb-0 flex flex-col">
+      <PaginatedDataTable
+        columns={columns}
+        data={data}
+        error={error}
+        isLoading={isPending}
+        isError={isError}
+      />
+    </div>
+  );
 };
 
-export default Shows;
+export const ShowsPage = FullPage(Shows);
+
+export default ShowsPage;
