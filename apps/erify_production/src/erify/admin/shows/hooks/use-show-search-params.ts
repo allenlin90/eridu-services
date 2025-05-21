88c337364c24482f2a7894fab68e14e6da41fdeb
allenlin <ht016746@gmail.com>
2025-05-21 21:17:11 +0700

    feat: erify admin can search shows by show_id

--- apps/erify_production/src/erify/admin/shows/hooks/use-show-search-params.ts
+++ apps/erify_production/src/erify/admin/shows/hooks/use-show-search-params.ts
@@ -8,6 +8,7 @@ export const useShowSearchParams = () => {
     return {
       params: {
         name: searchParams.get("name"),
+        show_id: searchParams.get("show_id"),
         brand_id: searchParams.get("brand_id"),
         start_time: searchParams.get("start_time"),
         end_time: searchParams.get("end_time"),
