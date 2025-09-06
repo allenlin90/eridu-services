ab41115516fcbcd2e0c6c53e5eb3ca6991145732
allenlin <ht016746@gmail.com>
2025-09-06 21:14:51 +0700

    chore: nestjs-pino init

--- apps/erify_api/src/main.ts
+++ apps/erify_api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   app.use(helmet());
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
