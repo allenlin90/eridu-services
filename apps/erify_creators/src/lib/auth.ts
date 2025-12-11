aac7fe44770f6d0edade668a9bd43118ce276fa9
allenlin <ht016746@gmail.com>
2025-12-11 13:13:42 +0700

    chore: sending including credentials

--- apps/erify_creators/src/lib/auth.ts
+++ apps/erify_creators/src/lib/auth.ts
@@ -5,6 +5,7 @@ import { setCachedToken } from '@/lib/api/token-store';
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_AUTH_URL,
   fetchOptions: {
+    credentials: 'include',
     onResponse: async (context) => {
       const token = context.response.headers.get('set-auth-jwt');
       if (token) {
